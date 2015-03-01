74a44bcbb1a90f8664e02f0ed3fb30174fba32c8
Rick Song <ricksong@squareup.com>
2015-03-01 12:13:38 -0800

    Crappy routes working

--- routes/mainRoutes.next.js
+++ routes/mainRoutes.next.js
@@ -1,4 +1,10 @@
 Router.route('/', function () {
   this.render('home');
   SEO.set({ title: 'Home -' + Meteor.App.NAME });
+}, {
+  name: 'home'
+});
+
+Router.route('/lobbies', function() {
+  this.render('lobbies');
 });
